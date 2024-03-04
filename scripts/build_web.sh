49ac187a64af1f9af42a4bdfc06cdc9237d7d8ba
Haled Odat <8566042+HalidOdat@users.noreply.github.com>
2024-03-05 00:30:13 +0100

    Implement WASM exmaples

--- scripts/build_web.sh
+++ scripts/build_web.sh
@@ -3,5 +3,6 @@
 source scripts/setup_emscripten_sdk.sh
 
 emcmake cmake . -B build-web
-cmake --build build-web
-cp -r web/* build-web
+cmake --build build-web -j15
+cp -rv web/* build-web
+cp -rv build-web/examples/Widget/ExampleWidget* build-web
