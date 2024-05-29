cbedf15b7529c38dfd7164d1e23b6f6c075f5c30
Haled Odat <8566042+HalidOdat@users.noreply.github.com>
2024-05-29 19:06:37 +0200

    Showcase all exmples in web version

--- scripts/build_web.sh
+++ scripts/build_web.sh
@@ -5,4 +5,6 @@ source scripts/setup_emscripten_sdk.sh
 emcmake cmake . -B build-web
 cmake --build build-web -j15
 cp -rv web/* build-web
+cp -rv build-web/examples/Calculator/Calculator* build-web
 cp -rv build-web/examples/Widget/ExampleWidget* build-web
+cp -rv build-web/examples/Renderer2D/ExampleRenderer2D* build-web
